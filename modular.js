7e7454d2ce49d77fc1f33c6c2c327e5f8f4117ae
Thomas <tcoducayen@gmail.com>
2019-02-21 00:44:20 -0800

    finished the module assignment finally

--- modular.js
+++ modular.js
@@ -1,13 +1,18 @@
-// prints a list of files in a given directory
-var myModule = require('./module.js');
+/*
+ * Main program, takes the reuired function and uses it to log
+ * all of the files from a given folder in a given path and list
+ * specific ones that have the desired ext
+*/
+var myModule = require('./module.js')
 
-var dirName = process.argv[2];
-var fileExt = "." + process.argv[3];
+var dirName = process.argv[2]
+var fileExt = process.argv[3]
 
 myModule(dirName,fileExt,function(err,list) {
-  if (err) return console.error(err);
-  //myModule filters by ext files
+  if (err)
+    return console.error(err)
+
   list.forEach(function(file) {
-    console.log(file);
+    console.log(file)
   })
-});
+})
--- module.js
+++ module.js
@@ -1,18 +1,14 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-
-module.exports = function(dirName,fileExt,callback) { //input data in modular.js
-  //access the fs.readdir function
+module.exports = function myModule(dirName,fileExt,callback) {
+  fileExt = '.' + fileExt
   fs.readdir(dirName,function(err,list) {
-      if (err) return callback(err); // sends error to initial callback
-      // filter through all of the files
-      var filter = ;
-      list.foreach(function(file) {
-        var ext = path.extname(file);
-        if (ext === fileExt) {
-          console.log(file);
-        };
-      });
-  });
-});
+      if (err)
+        return callback(err) // early return=
+      list = list.filter( function(file) {
+        return path.extname(file) === fileExt
+      })
+      callback(null,list)
+  })
+}
